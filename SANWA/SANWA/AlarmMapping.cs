d7d8993e38b91f085b29c02dee8f25d41a9f15d0
JevonsChan <jevons.zhan@gmail.com>
2018-08-09 17:39:39 +0800

    Add ATEL Decoder && Alarm Message

--- SANWA/SANWA/AlarmMapping.cs
+++ SANWA/SANWA/AlarmMapping.cs
@@ -224,5 +224,97 @@ namespace SANWA.Utility
 
             return alarm;
         }
+
+
+        /// <summary>
+        /// Get alarm message only ATEL
+        /// </summary>
+        /// <param name="eqp_type"> 設備種類 </param>
+        /// <param name="Command"> 輸入命令 </param>
+        /// <param name="error_message"> 錯誤訊息 </param>
+        /// <returns></returns>
+        public AlarmMessage GetATEL(string eqp_type, string Command, string error_message)
+        {
+            AlarmMessage alarm;
+            DataTable dtTemp;
+            string supplier = "ATEL";
+            string strAlarmType = string.Empty;
+            string strAlarmCode = string.Empty;
+            string strAlarmAxis = string.Empty;
+            string strAlarmAxisEnglish = string.Empty;
+            string strSql = string.Empty;
+            DBUtil dBUtil = new DBUtil();
+
+            try
+            {
+                alarm = new AlarmMessage();
+
+                var query = (from a in dtCode.AsEnumerable()
+                             where a.Field<string>("node_type") == eqp_type.ToUpper()
+                                && a.Field<string>("vendor") == supplier.ToUpper()
+                                && Command.Contains(a.Field<string>("code_type"))
+                                && a.Field<string>("code_id") == error_message.ToUpper()
+                             select a).ToList();
+
+                if (query.Count > 0)
+                {
+                    dtTemp = query.CopyToDataTable();
+                    strAlarmType = dtTemp.Rows[0]["alarm_code_id"].ToString();
+                    strAlarmCode = dtTemp.Rows[0]["Code_ID"].ToString();
+                    alarm.CodeID = string.Format("{0}{1}", strAlarmType, strAlarmCode);
+                    alarm.IsStop = dtTemp.Rows[0]["Is_stop"].ToString() == "Y" ? true : false;
+
+                    alarm.Code_Type = dtTemp.Rows[0]["Code_Type"].ToString();
+                    alarm.Code_Name = dtTemp.Rows[0]["Code_Name"].ToString();
+                    alarm.Code_Cause = strAlarmAxis == string.Empty ? dtTemp.Rows[0]["Code_Desc"].ToString() : strAlarmAxis + " " + dtTemp.Rows[0]["Code_Desc"].ToString();
+                    alarm.Code_Cause_English = strAlarmAxisEnglish == string.Empty ? dtTemp.Rows[0]["Code_Desc_EN"].ToString() : strAlarmAxisEnglish + " " + dtTemp.Rows[0]["Code_Desc_EN"].ToString();
+                }
+                else
+                {
+                    throw new Exception(string.Format("SANWA.Utility.AlarmMapping\r\nException: {0} {1} Alarm type not exists.", supplier.ToUpper(), eqp_type.ToUpper()));
+                }
+
+                query = (from a in dtAlarmEvent.AsEnumerable()
+                         where a.Field<string>("Device_Name") == supplier.ToUpper()
+                            && a.Field<string>("alarm_no") == strAlarmCode
+                         select a).ToList();
+
+                if (query.Count > 0)
+                {
+                    dtTemp = query.CopyToDataTable();
+                }
+                else
+                {
+                    dtTemp = new DataTable();
+                }
+
+                if (dtTemp.Rows.Count > 0)
+                {
+                    alarm.LED_Red = dtTemp.Rows[0]["led_red"].ToString();
+                    alarm.LED_Yellow = dtTemp.Rows[0]["led_yellow"].ToString();
+                    alarm.LED_Green = dtTemp.Rows[0]["led_green"].ToString();
+                    alarm.LED_Bule = dtTemp.Rows[0]["led_bule"].ToString();
+                    alarm.Buzzer01 = dtTemp.Rows[0]["buzzer_01"].ToString();
+                    alarm.Buzzer02 = dtTemp.Rows[0]["buzzer_02"].ToString();
+                    alarm.IsStop = dtTemp.Rows[0]["Is_stop"].ToString() == "Y" ? true : false;
+                }
+                else
+                {
+                    alarm.LED_Red = "N";
+                    alarm.LED_Yellow = "N";
+                    alarm.LED_Green = "N";
+                    alarm.LED_Bule = "N";
+                    alarm.Buzzer01 = "N";
+                    alarm.Buzzer02 = "N";
+                    alarm.IsStop = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+
+            return alarm;
+        }
     }
 }
