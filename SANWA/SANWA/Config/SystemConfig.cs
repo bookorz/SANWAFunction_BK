b33c3e7c5f286a4c8aaed781da83da9deba45e01
ethan_huang <ethan_huang@sanwa-eng.com.tw>
2018-08-14 14:26:02 +0800

    add mapping data setting

--- SANWA/SANWA/Config/SystemConfig.cs
+++ SANWA/SANWA/Config/SystemConfig.cs
@@ -17,6 +17,7 @@ namespace SANWA.Utility.Config
         public string SystemMode { get; set; }
         public string OCR1ExePath { get; set; }
         public string OCR2ExePath { get; set; }
+        public string MappingData { get; set; }
 
         public static SystemConfig Get()
         {
