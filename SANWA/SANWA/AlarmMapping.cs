6a460dbe69708dc75fc54fe8b0d2bde6ac8eca06
JevonsChan <jevons.zhan@gmail.com>
2018-09-13 15:56:33 +0800

    Update view & function

--- SANWA/SANWA/AlarmMapping.cs
+++ SANWA/SANWA/AlarmMapping.cs
@@ -15,7 +15,6 @@ namespace SANWA.Utility
     public class AlarmMapping
     {
         private DataTable dtCode;
-        private DataTable dtAlarmEvent;
         private static readonly ILog logger = LogManager.GetLogger(typeof(AlarmMapping));
 
         public AlarmMapping()
@@ -23,24 +22,23 @@ namespace SANWA.Utility
             ContainerSet containerSet;
             string strSql = string.Empty;
             DBUtil dBUtil = new DBUtil();
+            Dictionary<string, object> keyValues = new Dictionary<string, object>();
             try
             {
                 containerSet = new ContainerSet();
 
                 dtCode = new DataTable();
 
-                strSql = "SELECT * FROM view_alarm_code order by alarm_code_id asc";
+                strSql = "SELECT * FROM view_alarm_code WHERE equipment_model_id = @equipment_model_id";
 
-                dtCode = dBUtil.GetDataTable(strSql, null);
+                keyValues.Add("@equipment_model_id", Config.SystemConfig.Get().SystemMode);
+
+                dtCode = dBUtil.GetDataTable(strSql, keyValues);
 
                 if (dtCode.Rows.Count < 0)
                 {
                     throw new Exception("SANWA.Utility.AlarmMapping\r\nException: Code List not exists.");
                 }
-
-                strSql = "SELECT * FROM alarm_event_config WHERE active = 'Y'";
-
-                dtAlarmEvent = dBUtil.GetDataTable(strSql, null);
 
             }
             catch (Exception ex)
@@ -62,13 +60,11 @@ namespace SANWA.Utility
         /// <param name="eqp_type"> 設備種類 </param>
         /// <param name="error_message"> 錯誤訊息 </param>
         /// <returns></returns>
-        public AlarmMessage Get(string supplier, string eqp_type, string error_message)
+        public AlarmMessage Get(string supplier, string eqp_type, string address, string error_message)
         {
             AlarmMessage alarm;
             DataTable dtTemp;
 
-            string strAlarmType = string.Empty;
-            string strAlarmCode = string.Empty;
             string strAlarmAxis = string.Empty;
             string strAlarmAxisEnglish = string.Empty;
             string strCode28 = string.Empty;
@@ -123,12 +119,8 @@ namespace SANWA.Utility
                         break;
 
                     case "TDK":
-
-                        strErrorCode = error_message;
-
-                        break;
-
                     case "KAWASAKI":
+                    case "ATEL":
 
                         strErrorCode = error_message;
 
@@ -164,79 +156,23 @@ namespace SANWA.Utility
                 var query = (from a in dtCode.AsEnumerable()
                              where a.Field<string>("node_type") == eqp_type.ToUpper()
                                 && a.Field<string>("vendor") == supplier.ToUpper()
-                                && a.Field<string>("code_id").ToUpper() == strErrorCode.ToUpper()
+                                && a.Field<string>("return_code").ToUpper() == strErrorCode.ToUpper()
+                                && a.Field<Int64>("device_address_id") == Convert.ToInt64(address)
                              select a).ToList();
 
                 if (query.Count > 0)
                 {
                     dtTemp = query.CopyToDataTable();
-                    strAlarmType = dtTemp.Rows[0]["alarm_code_id"].ToString();
-                    strAlarmCode = dtTemp.Rows[0]["code_id"].ToString();
-                    alarm.CodeID = string.Format("{0}{1}", strAlarmType, strAlarmCode);
+                    alarm.CodeID = dtTemp.Rows[0]["sys_code_id"].ToString();
                     alarm.IsStop = dtTemp.Rows[0]["Is_stop"].ToString() == "Y" ? true : false;
-                }
-                else
-                {
-                    throw new Exception(string.Format("SANWA.Utility.AlarmMapping\r\nException: {0} {1} Alarm type not exists.", supplier.ToUpper(), eqp_type.ToUpper()));
-                }
-
-
-                query = null;
-                query = (from a in dtCode.AsEnumerable()
-                         where a.Field<string>("node_type") == eqp_type.ToUpper()
-                            && a.Field<string>("vendor") == supplier.ToUpper()
-                            && a.Field<string>("alarm_code_id") == strAlarmType
-                            && a.Field<string>("Code_ID") == strAlarmCode
-                         select a).ToList();
-
-                dtTemp = query.CopyToDataTable();
-
-                if (query.Count > 0)
-                {
-                    dtTemp = query.CopyToDataTable();
-                    alarm.Code_Type = dtTemp.Rows[0]["Code_Type"].ToString();
+                    alarm.Code_Type = dtTemp.Rows[0]["return_code_type"].ToString();
                     alarm.Code_Name = dtTemp.Rows[0]["Code_Name"].ToString();
                     alarm.Code_Cause = strAlarmAxis == string.Empty ? dtTemp.Rows[0]["Code_Desc"].ToString() : strAlarmAxis + " " + dtTemp.Rows[0]["Code_Desc"].ToString();
                     alarm.Code_Cause_English = strAlarmAxisEnglish == string.Empty ? dtTemp.Rows[0]["Code_Desc_EN"].ToString() : strAlarmAxisEnglish + " " + dtTemp.Rows[0]["Code_Desc_EN"].ToString();
                 }
                 else
                 {
-                    throw new Exception("SANWA.Utility.AlarmMapping\r\nException: Alarm Code not exists.");
-                }
-
-                query = (from a in dtAlarmEvent.AsEnumerable()
-                        where a.Field<string>("Device_Name") == supplier.ToUpper()
-                           && a.Field<string>("alarm_no") == strAlarmCode
-                         select a).ToList();
-
-                if (query.Count > 0)
-                {
-                    dtTemp = query.CopyToDataTable();
-                }
-                else
-                {
-                    dtTemp = new DataTable();
-                }
-
-                if (dtTemp.Rows.Count > 0)
-                {
-                    alarm.LED_Red = dtTemp.Rows[0]["led_red"].ToString();
-                    alarm.LED_Yellow = dtTemp.Rows[0]["led_yellow"].ToString();
-                    alarm.LED_Green = dtTemp.Rows[0]["led_green"].ToString();
-                    alarm.LED_Bule = dtTemp.Rows[0]["led_bule"].ToString();
-                    alarm.Buzzer01 = dtTemp.Rows[0]["buzzer_01"].ToString();
-                    alarm.Buzzer02 = dtTemp.Rows[0]["buzzer_02"].ToString();
-                    alarm.IsStop = dtTemp.Rows[0]["Is_stop"].ToString() == "Y" ? true : false;
-                }
-                else
-                {
-                    alarm.LED_Red = "N";
-                    alarm.LED_Yellow = "N";
-                    alarm.LED_Green = "N";
-                    alarm.LED_Bule = "N";
-                    alarm.Buzzer01 = "N";
-                    alarm.Buzzer02 = "N";
-                    alarm.IsStop = false;
+                    throw new Exception(string.Format("SANWA.Utility.AlarmMapping\r\nException: {0} {1} Alarm type not exists.", supplier.ToUpper(), eqp_type.ToUpper()));
                 }
             }
             catch (Exception ex)
@@ -247,95 +183,60 @@ namespace SANWA.Utility
             return alarm;
         }
 
-        /// <summary>
-        /// Get alarm message only ATEL
-        /// </summary>
-        /// <param name="eqp_type"> 設備種類 </param>
-        /// <param name="Command"> 輸入命令 </param>
-        /// <param name="error_message"> 錯誤訊息 </param>
-        /// <returns></returns>
-        public AlarmMessage GetATEL(string eqp_type, string Command, string error_message)
-        {
-            AlarmMessage alarm;
-            DataTable dtTemp;
-            string supplier = "ATEL";
-            string strAlarmType = string.Empty;
-            string strAlarmCode = string.Empty;
-            string strAlarmAxis = string.Empty;
-            string strAlarmAxisEnglish = string.Empty;
-            string strSql = string.Empty;
-            DBUtil dBUtil = new DBUtil();
+        ///// <summary>
+        ///// Get alarm message only ATEL
+        ///// </summary>
+        ///// <param name="eqp_type"> 設備種類 </param>
+        ///// <param name="Command"> 輸入命令 </param>
+        ///// <param name="error_message"> 錯誤訊息 </param>
+        ///// <returns></returns>
+        //public AlarmMessage GetATEL(string eqp_type, string Command, string error_message)
+        //{
+        //    AlarmMessage alarm;
+        //    DataTable dtTemp;
+        //    string supplier = "ATEL";
+        //    string strAlarmType = string.Empty;
+        //    string strAlarmCode = string.Empty;
+        //    string strAlarmAxis = string.Empty;
+        //    string strAlarmAxisEnglish = string.Empty;
+        //    string strSql = string.Empty;
+        //    DBUtil dBUtil = new DBUtil();
 
-            try
-            {
-                alarm = new AlarmMessage();
+        //    try
+        //    {
+        //        alarm = new AlarmMessage();
 
-                var query = (from a in dtCode.AsEnumerable()
-                             where a.Field<string>("node_type") == eqp_type.ToUpper()
-                                && a.Field<string>("vendor") == supplier.ToUpper()
-                                && Command.Contains(a.Field<string>("code_type"))
-                                && a.Field<string>("code_id") == error_message.ToUpper()
-                             select a).ToList();
+        //        var query = (from a in dtCode.AsEnumerable()
+        //                     where a.Field<string>("node_type") == eqp_type.ToUpper()
+        //                        && a.Field<string>("vendor") == supplier.ToUpper()
+        //                        && Command.Contains(a.Field<string>("code_type"))
+        //                        && a.Field<string>("code_id") == error_message.ToUpper()
+        //                     select a).ToList();
 
-                if (query.Count > 0)
-                {
-                    dtTemp = query.CopyToDataTable();
-                    strAlarmType = dtTemp.Rows[0]["alarm_code_id"].ToString();
-                    strAlarmCode = dtTemp.Rows[0]["Code_ID"].ToString();
-                    alarm.CodeID = string.Format("{0}{1}", strAlarmType, strAlarmCode);
-                    alarm.IsStop = dtTemp.Rows[0]["Is_stop"].ToString() == "Y" ? true : false;
+        //        if (query.Count > 0)
+        //        {
+        //            dtTemp = query.CopyToDataTable();
+        //            strAlarmType = dtTemp.Rows[0]["alarm_code_id"].ToString();
+        //            strAlarmCode = dtTemp.Rows[0]["Code_ID"].ToString();
+        //            alarm.CodeID = string.Format("{0}{1}", strAlarmType, strAlarmCode);
+        //            alarm.IsStop = dtTemp.Rows[0]["Is_stop"].ToString() == "Y" ? true : false;
 
-                    alarm.Code_Type = dtTemp.Rows[0]["Code_Type"].ToString();
-                    alarm.Code_Name = dtTemp.Rows[0]["Code_Name"].ToString();
-                    alarm.Code_Cause = strAlarmAxis == string.Empty ? dtTemp.Rows[0]["Code_Desc"].ToString() : strAlarmAxis + " " + dtTemp.Rows[0]["Code_Desc"].ToString();
-                    alarm.Code_Cause_English = strAlarmAxisEnglish == string.Empty ? dtTemp.Rows[0]["Code_Desc_EN"].ToString() : strAlarmAxisEnglish + " " + dtTemp.Rows[0]["Code_Desc_EN"].ToString();
-                }
-                else
-                {
-                    throw new Exception(string.Format("SANWA.Utility.AlarmMapping\r\nException: {0} {1} Alarm type not exists.", supplier.ToUpper(), eqp_type.ToUpper()));
-                }
+        //            alarm.Code_Type = dtTemp.Rows[0]["Code_Type"].ToString();
+        //            alarm.Code_Name = dtTemp.Rows[0]["Code_Name"].ToString();
+        //            alarm.Code_Cause = strAlarmAxis == string.Empty ? dtTemp.Rows[0]["Code_Desc"].ToString() : strAlarmAxis + " " + dtTemp.Rows[0]["Code_Desc"].ToString();
+        //            alarm.Code_Cause_English = strAlarmAxisEnglish == string.Empty ? dtTemp.Rows[0]["Code_Desc_EN"].ToString() : strAlarmAxisEnglish + " " + dtTemp.Rows[0]["Code_Desc_EN"].ToString();
+        //        }
+        //        else
+        //        {
+        //            throw new Exception(string.Format("SANWA.Utility.AlarmMapping\r\nException: {0} {1} Alarm type not exists.", supplier.ToUpper(), eqp_type.ToUpper()));
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new Exception(ex.ToString());
+        //    }
 
-                query = (from a in dtAlarmEvent.AsEnumerable()
-                         where a.Field<string>("Device_Name") == supplier.ToUpper()
-                            && a.Field<string>("alarm_no") == strAlarmCode
-                         select a).ToList();
-
-                if (query.Count > 0)
-                {
-                    dtTemp = query.CopyToDataTable();
-                }
-                else
-                {
-                    dtTemp = new DataTable();
-                }
-
-                if (dtTemp.Rows.Count > 0)
-                {
-                    alarm.LED_Red = dtTemp.Rows[0]["led_red"].ToString();
-                    alarm.LED_Yellow = dtTemp.Rows[0]["led_yellow"].ToString();
-                    alarm.LED_Green = dtTemp.Rows[0]["led_green"].ToString();
-                    alarm.LED_Bule = dtTemp.Rows[0]["led_bule"].ToString();
-                    alarm.Buzzer01 = dtTemp.Rows[0]["buzzer_01"].ToString();
-                    alarm.Buzzer02 = dtTemp.Rows[0]["buzzer_02"].ToString();
-                    alarm.IsStop = dtTemp.Rows[0]["Is_stop"].ToString() == "Y" ? true : false;
-                }
-                else
-                {
-                    alarm.LED_Red = "N";
-                    alarm.LED_Yellow = "N";
-                    alarm.LED_Green = "N";
-                    alarm.LED_Bule = "N";
-                    alarm.Buzzer01 = "N";
-                    alarm.Buzzer02 = "N";
-                    alarm.IsStop = false;
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.ToString());
-            }
-
-            return alarm;
-        }
+        //    return alarm;
+        //}
     }
 }
